1ead65ababa78a77f8d7d7f4b606a4e87ba53eb0
Thenul-Ranmuthu <thenul.ranmuthu@gmail.com>
2026-08-25 16:45:26 +0530

    SCRUM-30 Refactor RegistrationController to improve role assignment and add debug claims endpoint

--- src/Controllers/RegistrationController.cs
+++ src/Controllers/RegistrationController.cs
@@ -1,61 +1,3 @@
-// using System.Security.Claims;
-// using Microsoft.AspNetCore.Authorization;
-// using Microsoft.AspNetCore.Mvc;
-// using SRC.Services.Interfaces;
-
-// namespace SRC.Controllers
-// {
-//     [ApiController]
-//     [Route("api/onboarding")]
-//     public class RegistrationController: ControllerBase
-//     {
-//         private readonly IAsgardeoRoleService _roleService;
-//         private readonly IConfiguration _config;
-
-//         public RegistrationController(IAsgardeoRoleService roleService, IConfiguration config)
-//         {
-//             _roleService = roleService;
-//             _config = config;
-//         }
-
-//         [HttpPost("select-role")]
-//         [Authorize]
-//         public async Task<IActionResult> SelectRole([FromBody] RoleSelectionRequest req)
-//         {
-//             var asgardeoUserId = User.FindFirstValue("sub");
-//             if (asgardeoUserId is null)
-//                 return Unauthorized();
-
-//             var displayName = User.FindFirstValue("name") ?? User.FindFirstValue("email") ?? asgardeoUserId;
-            
-//             // var email = User.Claims.FirstOrDefault(c => 
-//             //     c.Type.Contains("email", StringComparison.OrdinalIgnoreCase)
-//             // )?.Value;
-
-
-//             var roleId = req.Role switch
-//             {
-//                 "Driver" => _config["AsgardeoRoles:DriverRoleId"],
-//                 "Rider" => _config["AsgardeoRoles:RiderRoleId"],
-//                 _ => null
-//             };
-//             if (roleId is null)
-//                 return BadRequest("Invalid role");
-
-//             // if(email == null) return BadRequest("Email Not found in RegistrationController!!!");
-//             await _roleService.AssignRoleAsync(asgardeoUserId, displayName, roleId);
-//             return Ok();
-//         }
-//     }
-
-//     public record RoleSelectionRequest(string Role);
-// }
-
-
-
-
-
-
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -87,6 +29,7 @@ namespace SRC.Controllers
             var displayName = User.FindFirstValue("name") ?? User.FindFirstValue("email") ?? asgardeoUserId;
 
             var email = User.FindFirstValue("email");
+            var username = User.FindFirstValue("username") ?? asgardeoUserId;
 
             var roleId = req.Role switch
             {
@@ -98,10 +41,21 @@ namespace SRC.Controllers
                 return BadRequest("Invalid role");
 
             if(email == null) return BadRequest("Email Not found in RegistrationController!!!");
-            await _roleService.AssignRoleAsync(asgardeoUserId, email, roleId);
+            await _roleService.AssignRoleAsync(asgardeoUserId, username, roleId);
             return Ok();
         }
+
+        [HttpGet("debug-claims")]
+        [Authorize]
+        public IActionResult DebugClaims()
+        {
+            var claims = User.Claims.Select(c => new { c.Type, c.Value });
+            return Ok(claims);
+        }
     }
+
+
+    
 
     public record RoleSelectionRequest(string Role);
 }
