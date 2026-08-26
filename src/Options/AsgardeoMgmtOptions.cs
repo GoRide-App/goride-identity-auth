da933b96e6af78586dbc2d989c8b5e651abc1747
luhansa26 <praviniluhansa@gmail.com>
2026-08-26 08:09:06 +0530

    SCRUM-28[DevOps]: parameterize Asgardeo config via strongly-typed options

---
+++ src/Options/AsgardeoMgmtOptions.cs
@@ -0,0 +1,7 @@
+namespace GoRide.Api.Options;
+public class AsgardeoMgmtOptions
+
+{
+    public string ClientId { get; set; } = default!;
+    public string ClientSecret { get; set; } = default!;
+}
