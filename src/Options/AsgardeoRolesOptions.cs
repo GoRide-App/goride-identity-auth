4d04a404ee8080dd1b948a9f6a7c490268971d02
luhansa26 <praviniluhansa@gmail.com>
2026-08-26 08:09:06 +0530

    SCRUM-28[DevOps]: parameterize Asgardeo config via strongly-typed options

---
+++ src/Options/AsgardeoRolesOptions.cs
@@ -0,0 +1,6 @@
+namespace GoRide.Api.Options;
+public class AsgardeoRolesOptions
+{
+    public string RiderRoleId { get; set; } = default!;
+    public string DriverRoleId { get; set; } = default!;
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,30 @@
+using GoRide.Api.Options;   
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors();
+
+builder.Services
+    .AddOptions<AsgardeoOptions>()
+    .Bind(builder.Configuration.GetSection("Asgardeo"))
+    .ValidateDataAnnotations()
+    .Validate(o => !string.IsNullOrWhiteSpace(o.BaseUrl), "Asgardeo:BaseUrl is required")
+    .Validate(o => !string.IsNullOrWhiteSpace(o.ClientId), "Asgardeo:ClientId is required")
+    .Validate(o => !string.IsNullOrWhiteSpace(o.ClientSecret), "Asgardeo:ClientSecret is required")
+    .ValidateOnStart();
+
+builder.Services
+    .AddOptions<AsgardeoMgmtOptions>()
+    .Bind(builder.Configuration.GetSection("AsgardeoMgmt"))
+    .ValidateOnStart();
+
+builder.Services
+    .AddOptions<AsgardeoRolesOptions>()
+    .Bind(builder.Configuration.GetSection("AsgardeoRoles"))
+    .ValidateOnStart();
 
 var app = builder.Build();
 
